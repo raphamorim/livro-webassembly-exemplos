27ffc03a3290c2c81c759089832fd6172b6c3d1e
Raphael Amorim <rapha850@gmail.com>
2022-09-21 20:42:00 +0200

    fixup! ...

--- capitulo-5/vinculacao/numeros.rs
+++ capitulo-5/vinculacao/numeros.rs
@@ -1,7 +1,12 @@
-pub static NUMERO: u32 = 87654321;
 
-static VINTE: u32 = 20;
 
-pub extern fn retorna_vinte() -> &'static u32 {
-    &VINTE
+static DEZ: i32 = 10;
+#[export_name = "retorna_dez"]
+pub extern fn retorna_10() -> i32 {
+    DEZ
+}
+
+#[no_mangle]
+pub extern fn quadrado(x: i32) -> i32 {
+    x * x
 }
--- capitulo-5/vinculacao/vinculacao.rs
+++ capitulo-5/vinculacao/vinculacao.rs
@@ -1,4 +1,4 @@
-// rustc vinculacao.rs --target wasm32-unknown-unknown --crate-type=cdylib 
+// rustc vinculacao.rs --target wasm32-unknown-unknown --crate-type=cdylib -L ./
 
 extern "C" {
     #[link_name = "console_log"]
@@ -8,17 +8,24 @@ extern "C" {
     fn alert(x: i32) -> i32;
 }
 
-#[link(name = "quadrado")]
+#[link(name = "numeros", kind = "static")]
 extern "C" {
+    fn retorna_dez() -> i32;
+
     fn quadrado(x: i32) -> i32;
 }
 
 #[no_mangle]
 pub fn executar() {
     unsafe {
+        let dez = retorna_dez();
+
         log(20); // 20
         log(87654321); // 87654321
+        
+        log(dez); // 10
+        log(quadrado(dez)); // 100
 
-        alert(quadrado(85)); // 85 com window.alert
+        alert(85); // 85 com window.alert
     }
 }
